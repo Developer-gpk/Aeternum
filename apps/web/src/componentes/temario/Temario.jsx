d6328373b23674bdcfd1d2d84fe147384e03330a
Developer-gpk <dev.goplek@gmail.com>
2022-11-25 14:53:05 -0600

    prueba

--- apps/web/src/componentes/temario/Temario.jsx
+++ apps/web/src/componentes/temario/Temario.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import Image from 'next/image'
 import { Textura3 } from 'ui'
 import { buildImages } from '../../../libs/complementos'
 
 export default function Temario({ dias }){
+    const [ up, setUp ] = useState(false)
+    const algo = useRef()
+    const Animation = () =>{
+        const prueba = algo.current
+        if(!prueba) return
+        if(prueba.getBoundingClientRect().top <= 598){
+            setUp(true)
+        } else{
+            setUp(false)
+        }
+    }
+    useEffect(() =>{
+        Animation()
+        window.addEventListener('scroll', Animation)
+    }, [])
     return(
         <section className='block temario' id='temario'>
             <div className='textura'>
@@ -16,8 +31,8 @@ export default function Temario({ dias }){
                     <div className='flex-row'>
                         {dias?.map((dia, index) =>(
                             <div className='tarjeta' key={index}>
-                                <div className='imagen'>
-                                    <Image src={buildImages(dia?.imagen?.asset).url()} fill alt={`Dia ${index+1}`} />
+                                <div className='imagen' ref={algo}>
+                                    <Image src={buildImages(dia?.imagen?.asset).url()} fill alt={`Dia ${index+1}`} className={`${up ? "prueba" : ""}`}  />
                                 </div>
                                 <div className='dia'>Día {index+1}</div>
                                 <div className='title'>{dia?.title}</div>
