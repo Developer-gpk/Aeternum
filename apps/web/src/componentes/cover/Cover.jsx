af8d6f302a1b72ee9004107cc1ab464094e7dfef
Developer-gpk <dev.goplek@gmail.com>
2022-12-06 14:27:27 -0600

    update

--- apps/web/src/componentes/cover/Cover.jsx
+++ apps/web/src/componentes/cover/Cover.jsx
@@ -6,35 +6,33 @@ import { useInView } from "react-hook-inview"
 import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 export default function Cover({ title1, title2, title3}){
-    const [ref, inView] = useInView({ threshold: 0 });
+    const [ref, inView] = useInView({ threshold: 1 });
     return(
         <section className='block cover' id='cover'>
             <div className='textura1'>
                 <Image src={Textura1} fill alt='Textura 1 cover' />
             </div>
             <div className='holder'>
-                <div className='container-fluid'>
-                    <div className="texto-1" ref={ref}>
+                <div className='container-fluid' ref={ref}>
+                    <div className={`texto-1 ${inView ? "view" : ""}`}>
                         Comienza tu experiencia en el mundo de las
                     </div>
-                    <div className='texto-2'>
+                    <div className={`texto-2 ${inView ? "view" : ""}`}>
                         Criptomonedas y Blockchain con nuestro Crash Course.
                     </div>
-                    <div className='text-3'>
+                    <div className={`text-3 ${inView ? "view" : ""}`}>
                         Tu llave al mundo crypto.
                     </div>
                     <Link className={`${inView ? "view" : ""}`} to="contacto" spy={true} smooth={true} duration={1000} delay={0}>
                         Inscríbete al Crash Course
                     </Link>
-                        <div className='text-cover-1'>
-                            <div className={`cover-1 ${inView ? "view" : ""}`}></div>
-                        </div>
-                        <div className='text-cover-2'>
-                            <div className={`cover-2 ${inView ? "view" : ""}`}></div>
-                        </div>
-                        <div className={`text-cover-3 ${inView ? "view" : ""}`}>
-                            
-                        </div>
+                    <div className='text-cover-1'>
+                        <div className={`cover-1 ${inView ? "view" : ""}`}></div>
+                    </div>
+                    <div className='text-cover-2'>
+                        <div className={`cover-2 ${inView ? "view" : ""}`}></div>
+                    </div>
+                    <div className={`text-cover-3 ${inView ? "view" : ""}`}></div>
                     
                 </div>
             </div>
