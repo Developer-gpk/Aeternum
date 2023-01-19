b40b326e10654c365ae8bb10b7a57b16ccfc72a5
Developer-gpk <dev.goplek@gmail.com>
2023-01-19 15:44:03 -0600

    update

--- apps/web/pages/_app.jsx
+++ apps/web/pages/_app.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Script from 'next/script';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 import '../src/styles/global.scss'
@@ -8,10 +9,22 @@ import "swiper/css/navigation"
 
 export default function MyApp({ Component, pageProps }) {
     return(
-        <PayPalScriptProvider options={{ "client-id": "AYMprwST6h7ZarJYCALE4AjhOg8Xilbls21O109JGDeM0V5AFDeuQW8Z18wI-jj3bOvM6hzLgEBeaJ05", currency: "MXN" }}>
-            <ParallaxProvider>
-                <Component {...pageProps} />
-            </ParallaxProvider>
-        </PayPalScriptProvider>
+        <>
+            <Script id='analytics-cdn' strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-BZ1SLQEZEY`} />
+            <Script id='script-analytics' strategy="lazyOnload">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                   
+                    gtag('config', 'G-BZ1SLQEZEY');
+                `}
+            </Script>
+            <PayPalScriptProvider options={{ "client-id": "AYMprwST6h7ZarJYCALE4AjhOg8Xilbls21O109JGDeM0V5AFDeuQW8Z18wI-jj3bOvM6hzLgEBeaJ05", currency: "MXN" }}>
+                <ParallaxProvider>
+                    <Component {...pageProps} />
+                </ParallaxProvider>
+            </PayPalScriptProvider> 
+        </>
     )
 }
